a7bb5139dbcd8cf72d6e873cd765c59551bae554
Lordputin404 <amarkujur4583@gmail.com>
2026-01-30 08:46:08 +0530

    munch:Initialize @yaap

--- yaap_munch.mk
+++ yaap_munch.mk
@@ -9,12 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-PRODUCT_NAME := lineage_munch
+TARGET_BUILD_GAPPS := true
+
+PRODUCT_NAME := yaap_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Poco
