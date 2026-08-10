1c8cdb4b81f619bfe079b39fc94ebeac3d4a1704
Lordputin404 <amarkujur4583@gmail.com>
2026-08-10 10:46:42 +0000

    munch: Add ViPER4AndroidFX support

--- device.mk
+++ device.mk
@@ -129,6 +129,10 @@ PRODUCT_PACKAGES += \
     libvisualizer \
     libvolumelistener
 
+# ViPER4Android FX
+PRODUCT_PACKAGES += \
+    ViPER4AndroidFX
+
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
